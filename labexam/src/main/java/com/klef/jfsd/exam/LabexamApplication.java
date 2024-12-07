d8715127db3f4a1be11965b530c8eb7bac30b586
Jayanth <126154147+jayanth2004klu@users.noreply.github.com>
2024-12-07 10:54:42 +0530

    Add files via upload

---
+++ labexam/src/main/java/com/klef/jfsd/exam/LabexamApplication.java
@@ -0,0 +1,12 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabexamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabexamApplication.class, args);
+    }
+}
